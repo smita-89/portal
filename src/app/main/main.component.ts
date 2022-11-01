29066516e041f0cca78de799b04342ec5bfb1ece
Renuka S <renuka_somagutta@mohawkind.com>
2022-11-01 20:03:40 +0530

    add validation form in school form

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -8,6 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 
   constructor() { }
+  logForm= new FormGroup({
+    schoolname: new FormControl(''),
+    schoolpassward: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    schoolshape: new FormControl('', [Validators.required]),
+    schoolemail: new FormControl('', [Validators.required, Validators.email]),
+
+  })
+  logUser(){
+    console.warn(this.logForm.value)
+  }
+  get schoolshape(){
+    return this.logForm.get('schoolshape')
+  }
+  get schoolemail(){
+    return this.logForm.get('schoolemail')
+  }
+
+  get schoolpassward(){
+    return this.logForm.get('schoolpassward')
+
+  }
 
   ngOnInit(): void {
   }
