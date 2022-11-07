1b1d6391a4549c44905a181d39c7b6a1cdd3bc3f
Renuka S <renuka_somagutta@mohawkind.com>
2022-11-07 12:02:10 +0530

    Remove unwanted line add solutions in file

--- src/app/solution-list/solution-list.component.ts
+++ src/app/solution-list/solution-list.component.ts
@@ -6,26 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./solution-list.component.css']
 })
 export class SolutionListComponent implements OnInit {
-  proctSo = [];
-  solutions= [
+
+  solutions = [
     {
       title: 'DISCOVER',
-      subtitle: ' LIFE INSURANCE',
+      subtitle: 'LIFE INSURANCE',
       desc: 'Financially protect what you value - your child’s future, retirement income and financial',
-      button: "Buye"
+      button: "BUY ONLINE"
     },
     {
-      title: 'DISCOVER2',
-      subtitle: ' 2LIFE INSURANCE',
-      desc: 'Financially protect what you value - your child’s future, retirement income and financial',
-      button: "Buye"
+      title: 'DISCOVER',
+      subtitle: 'HEALTH INSURANCE',
+      desc: 'Along with offering a comprehensive Health Insurance, we encourage you to live a healthy life and reward you for it.',
+      button: 'BUY ONLINE'
     },
     {
-      title: '44DISCOVER2',
+      title: 'DISCOVER',
+      subtitle: 'MULTIPLY WELLNESS',
+      desc: 'Join our unique Wellness program that rewards you for staying healthy from the day you sign up.',
+      button: 'VIEW PLANS'
+    },
+
+    {
+      title: 'DISCOVER',
+      subtitle: 'MOTOR INSURANCE',
+      desc: 'Choose from a host of Motor Insurance providers to protect your prized vehicle from accidents and theft.',
+      button: 'COMPAIRE AND BUY'
+    },
+    {
+      title: 'DISCOVER',
       subtitle: ' 2LIFE INSURANCE',
       desc: 'Financially protect what you value - your child’s future, retirement income and financial',
-      button: "Buye"
-    }
+      button: 'REQUEST A CALL BACK'
+    },
   ];
 
 
